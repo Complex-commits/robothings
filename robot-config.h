468ae8cfdc6fa1398bf3553bf26ed64904e11d50
JS <43799310+UnboundWarrior@users.noreply.github.com>
2018-12-06 15:31:35 -0600

    Create robot-config.h

---
+++ robot-config.h
@@ -0,0 +1,14 @@
+vex::brain Brain;
+vex::controller Controller1 = vex::controller();
+vex::controller Controller = vex::controller();
+vex::motor frontLeftMotor = vex::motor(vex::PORT1);
+vex::motor rearLeftMotor = vex::motor(vex::PORT2);
+vex::motor rearRightMotor = vex::motor(vex::PORT3,true);
+vex::motor frontRightMotor = vex::motor(vex::PORT4,true);
+vex::motor ArmMotorLeft = vex::motor(vex:: PORT5);
+vex::motor ArmMotorRight = vex::motor(vex:: PORT6,true);
+vex::motor climberLeft = vex::motor(vex:: PORT7);
+vex::motor climberRight = vex::motor(vex:: PORT8,true);
+
+
+
